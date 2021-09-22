4870f5f7772ca473f353017db1806f19060624f7
Marksteev N <91110308+MarksteevN@users.noreply.github.com>
2021-09-22 13:42:51 +0530

    Rock Paper Scissor Game

---
+++ RockPaperScissors/Helpers/Hand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockPaperScissor.Helpers
+{
+    public class Hand
+    {
+        public OptionRPS Selection { get; set; }
+        public OptionRPS WinsAgainst { get; set; }
+        public OptionRPS LossAgainst { get; set; }
+        public string Image { get; set; }
+
+        public GameStatus PlayAgainst(Hand opponentHand)
+        {
+            if (Selection == opponentHand.Selection)
+            {
+                return GameStatus.Draw;
+            }
+            if (LossAgainst == opponentHand.Selection)
+            {
+                return GameStatus.Loss;
+            }
+            return GameStatus.Victory;
+        }
+    }
+}
